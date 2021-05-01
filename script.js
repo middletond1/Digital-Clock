162cd60616612152e0bb2e8e5865187da52ef216
David Middleton <middleton.davida@gmail.com>
2021-04-30 21:20:34 -0400

    Added notes

--- script.js
+++ script.js
@@ -11,11 +11,15 @@ let hours = d.getHours();
 
 function setDay() {
     let displayDay = '';
+    incrementDate();
+//call the incrementDate function here so it runs first and sets the correct values for the other functions.
     if (minutes === 0 && seconds === 0 && hours === 0 && day === 6) {
         day = 0;
     } else if (minutes === 0 && seconds === 0 && hours === 0) {
         day++;
     } else day;
+//checks if midnight on Saturday and resets day variable to 0(sunday). If midnight but not saturday, increments on day variable. 
+//Otherwise returns day's original value.
     switch (day) {
         case 0:
           displayDay = "Sunday";
@@ -39,35 +43,12 @@ function setDay() {
           displayDay = "Saturday";
           break;
       }
+//Sets the displayDay empty string variable to a day of the week.
     return displayDay;
 }
 
 function setMonth() {
     let displayMonth = '';
-    if (minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 0 || 
-        minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 2 || 
-        minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 4 || 
-        minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 6 || 
-        minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 7 || 
-        minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 9) {
-        date = 1;
-        month++;
-    } else if (minutes === 0 && seconds === 0 && hours === 0 && date === 30 && month === 3 || 
-        minutes === 0 && seconds === 0 && hours === 0 && date === 30 && month === 5 || 
-        minutes === 0 && seconds === 0 && hours === 0 && date === 30 && month === 8 || 
-        minutes === 0 && seconds === 0 && hours === 0 && date === 30 && month === 10) {
-        date = 1;
-        month++;
-    } else if (minutes === 0 && seconds === 0 && hours === 0 && date === 28 && month === 1) {
-        date = 1;
-        month++;
-    } else if (minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 11) {
-        date = 1;
-        month = 0;
-        year++;
-    } else if (minutes === 0 && seconds === 0 && hours === 0 ) {
-        date++;
-    }
         switch (month) {
             case 0:
                 displayMonth = 'January';
@@ -108,6 +89,7 @@ function setMonth() {
         }
     return displayMonth;
 }
+//returns a string with the calendar month based on the value of the month variable.
 
 function setDate() {
     let displayDate = '';
@@ -122,6 +104,38 @@ function setDate() {
     }
     return displayDate;
 }
+//Adds the correct suffix to the date.
+
+function incrementDate() {
+    if (minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 0 || 
+        minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 2 || 
+        minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 4 || 
+        minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 6 || 
+        minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 7 || 
+        minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 9) {
+        date = 1;
+        month++;
+    } else if (minutes === 0 && seconds === 0 && hours === 0 && date === 30 && month === 3 || 
+        minutes === 0 && seconds === 0 && hours === 0 && date === 30 && month === 5 || 
+        minutes === 0 && seconds === 0 && hours === 0 && date === 30 && month === 8 || 
+        minutes === 0 && seconds === 0 && hours === 0 && date === 30 && month === 10) {
+        date = 1;
+        month++;
+    } else if (minutes === 0 && seconds === 0 && hours === 0 && date === 28 && month === 1) {
+        date = 1;
+        month++;
+    } else if (minutes === 0 && seconds === 0 && hours === 0 && date === 31 && month === 11) {
+        date = 1;
+        month = 0;
+        year++;
+    } else if (minutes === 0 && seconds === 0 && hours === 0 ) {
+        date++;
+    }
+}
+//Checks to see if it is the last day of the month from January to November.
+//If it is, function sets the date to the 1st and increments the month. 
+//If it is December, it sets the date to the 1st, resets month to 0, and increments the year.
+//If it is midnight but not the end of the month, increments on the date. 
 
 function incrementSeconds() {
     if (seconds < 59) {
@@ -129,6 +143,9 @@ function incrementSeconds() {
     } else seconds = 0
     return seconds.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
 }
+//Checks to see if seconds variable is less than 59. If so, increments seconds.
+//If 59, it resets seconds to 0.
+//Returns seconds with 2 digits minimum.
 
 function incrementMinutes() {
     if (seconds === 59 && minutes < 59) {
@@ -138,6 +155,9 @@ function incrementMinutes() {
     } else minutes;
     return minutes.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
 }
+//Checks to see if it is the end of the minute and if the minutes variable is less than 59. If so, increments minutes.
+//If seconds and minutes are both 59, resets minutes to 0.
+//Returns minutes with 2 digits minimum.
 
 function incrementHours() {
     if (minutes === 59 && seconds === 59 && hours < 23) {
@@ -147,6 +167,8 @@ function incrementHours() {
     } 
     return hours;
 }
+//Checks to see if it is the end of the hour and if the hours variable is less than 23. If so, increments hours.
+//If seconds and minutes are both 59 and hours are 23 (end of the day), resets hours to 0.
 //Setting a variable in these increment functions will cause them to reset after a minute loop.
 
 function hoursToStandard() {
@@ -191,6 +213,8 @@ function hoursToStandard() {
     }
     return displayHours.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
 }
+//Takes the hours returned from the incrementHours function and returns a number between 1-12, based on standard hour clocks.
+//Returns a number with 2 minimum digits.
 
 function setLatin() {
     let latin = 'AM';
@@ -199,16 +223,22 @@ function setLatin() {
     };
     return latin;
 }
+//Checks if hours is greater than 11. If so, returns a PM variable. If 11 or less, returns AM.
 
 function printTime() {
     return docTime.textContent = `${hoursToStandard()}:${incrementMinutes()}:${incrementSeconds()} ${setLatin()}`
 }
+//Prints the time string to the Doc.
 
 function printDate() {
     return docDate.textContent = `${setDay()}, ${setMonth()} ${setDate()} ${year}`
 }
+//Prints the date string to the doc.
 
 printTime();
 printDate();
+//Prints the initial values of time and date to the doc on start up.
+
 setInterval(printTime, 1000);
 setInterval(printDate, 1000);
+//Runs the printTime and printDate functions every second.
